e563c15b994a4eab579893793be4d5e70222d555
James Hugman <james@hugman.tv>
2021-12-01 13:44:33 +0000

    Add build scripts

--- components/support/nimbus-fml/scripts/build-dist.sh
+++ components/support/nimbus-fml/scripts/build-dist.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-
-targets="x86_64-unknown-linux-musl x86_64-apple-darwin"
+targets="aarch64-apple-darwin x86_64-unknown-linux-musl x86_64-apple-darwin"
 dry_run=0
 
-root_dir=../../..
+root_dir=$(dirname $0)/../../../..
+fml_dir=$root_dir/components/support/nimbus-fml
 target_dir=$root_dir/target
-filename=$(basename $(pwd))
+filename=nimbus-fml
 dist_file=${filename}.zip
 
-echo "Installing tools for cross compiling"
+prompt='$'
+echo "## Installing tools for cross compiling"
 install_musl_cross="brew install filosottile/musl-cross/musl-cross"
 cargo_clean="cargo clean"
 if (( $dry_run == 0 )) ; then
     $install_musl_cross
     $cargo_clean
 else
-    echo "-> $install_musl_cross"
-    echo "-> $cargo_clean"
+    echo "$prompt $install_musl_cross"
+    echo "$prompt $cargo_clean"
 fi
 
 # Now we need to add a linker
@@ -26,25 +27,28 @@ zip_cmd="zip $(pwd)/$dist_file "
 
 for TARGET in $targets ; do
     echo
-    echo "Cross compiling for $TARGET"
+    echo "## Cross compiling for $TARGET"
     rustup="rustup target add $TARGET"
     cargo_build="cargo build --release --target $TARGET"
 
     if (( $dry_run == 0 )) ; then
+
         $rustup
-        $cargo_build
+        (cd $fml_dir; $cargo_build)
+
     else
-        echo "-> $rustup"
-        echo "-> $cargo_build"
+        echo "$prompt $rustup"
+        echo "$prompt (cd $fml_dir; $cargo_build )"
     fi
 
     zip_cmd="$zip_cmd $TARGET/release/$filename"
 
 done
-
+echo
+echo "## Preparing dist archive"
 cmd="(cd $target_dir ; $zip_cmd )"
 if (( $dry_run == 0 )) ; then
     (cd $target_dir ; $zip_cmd )
 else
-    echo "-> $cmd"
+    echo "$prompt $cmd"
 fi
