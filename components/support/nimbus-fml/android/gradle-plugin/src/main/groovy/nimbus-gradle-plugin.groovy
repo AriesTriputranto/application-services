84a32ae73184138a9d35260007b207eff1b95557
James Hugman <james@hugman.tv>
2021-12-02 16:07:07 +0000

    Groovy plugin compiles

--- components/support/nimbus-fml/android/gradle-plugin/src/main/groovy/nimbus-gradle-plugin.groovy
+++ components/support/nimbus-fml/android/gradle-plugin/src/main/groovy/nimbus-gradle-plugin.groovy
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.experiments.nimbus-gradle-plugin
+//package org.mozilla.experiments.nimbus-gradle-plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
